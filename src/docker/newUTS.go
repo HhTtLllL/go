568fca8692c6881c36cc7134b10962b587167376
HhTtLllL <1430249706@qq.com>
2020-08-24 21:16:09 +0800

    update

--- src/docker/newUTS.go
+++ src/docker/newUTS.go
@@ -9,11 +9,31 @@ import (
 
 func main() {
 
-	cmd := exec.Command("sh")
+	cmd := exec.Command("ls","-l")
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:                 syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID,
+		Cloneflags:                 syscall.CLONE_NEWUTS, //| syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
+			//syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+
+		/*UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      syscall.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      syscall.Getgid(),
+				Size:        1,
+			},
+		},*/
 	}
+
+	//cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1) }
+
+
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -23,4 +43,6 @@ func main() {
 
 		log.Fatal(err)
 	}
+
+	os.Exit(-1)
 }
